2a6893975cac3da65dd9e174ac54cb71751c04fd
serikkabi <96568577+serikkabi@users.noreply.github.com>
2022-02-21 01:42:16 +0600

    Add files via upload

---
+++ Mustafa, Serik/main.js
@@ -0,0 +1,8 @@
+var yourName = prompt("What is your name?");
+alert("Hello " + yourName.slice(0,1).toUpperCase() + yourName.slice(1, yourName.length).toLowerCase() + "!!!");
+var audio = new Audio('audio_file.mp3');
+audio.play();
+function play() {
+  var audio = new Audio('qwerty.mp3');
+  audio.play();
+}
